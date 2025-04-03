1c9e373c28f1abd9efaad79781ca75c29fc66c61
Sukki Moon <sukki.moon@schperics.com>
2025-04-03 13:45:20 +0900

    feat: binance api 테스트용 목적 수정

--- example/src/generic_private_request/main.cpp
+++ example/src/generic_private_request/main.cpp
@@ -18,30 +18,25 @@ using ::ccapi::SessionOptions;
 using ::ccapi::UtilSystem;
 using ::ccapi::UtilTime;
 int main(int argc, char** argv) {
-  if (UtilSystem::getEnvAsString("COINBASE_API_KEY").empty()) {
-    std::cerr << "Please set environment variable COINBASE_API_KEY" << std::endl;
+  if (UtilSystem::getEnvAsString("BINANCE_API_KEY").empty()) {
+    std::cerr << "Please set environment variable BINANCE_API_KEY" << std::endl;
     return EXIT_FAILURE;
   }
-  if (UtilSystem::getEnvAsString("COINBASE_API_SECRET").empty()) {
-    std::cerr << "Please set environment variable COINBASE_API_SECRET" << std::endl;
-    return EXIT_FAILURE;
-  }
-  if (UtilSystem::getEnvAsString("COINBASE_API_PASSPHRASE").empty()) {
-    std::cerr << "Please set environment variable COINBASE_API_PASSPHRASE" << std::endl;
+  if (UtilSystem::getEnvAsString("BINANCE_API_SECRET").empty()) {
+    std::cerr << "Please set environment variable BINANCE_API_SECRET" << std::endl;
     return EXIT_FAILURE;
   }
   SessionOptions sessionOptions;
   SessionConfigs sessionConfigs;
   MyEventHandler eventHandler;
   Session session(sessionOptions, sessionConfigs, &eventHandler);
-  Request request(Request::Operation::GENERIC_PRIVATE_REQUEST, "coinbase", "", "Get all fills");
+  Request request(Request::Operation::GENERIC_PRIVATE_REQUEST, "binance", "", "Get Account Info");
   request.appendParam({
       {"HTTP_METHOD", "GET"},
-      {"HTTP_PATH", "/fills"},
-      {"HTTP_QUERY_STRING", "product_id=BTC-USD"},
+      {"HTTP_PATH", "/api/v3/account"},
   });
   session.sendRequest(request);
-  std::this_thread::sleep_for(std::chrono::seconds(10));
+  std::this_thread::sleep_for(std::chrono::seconds(2));
   session.stop();
   std::cout << "Bye" << std::endl;
   return EXIT_SUCCESS;
--- example/src/generic_public_request/main.cpp
+++ example/src/generic_public_request/main.cpp
@@ -21,26 +21,26 @@ int main(int argc, char** argv) {
   MyEventHandler eventHandler;
   Session session(sessionOptions, sessionConfigs, &eventHandler);
   {
-    Request request(Request::Operation::GENERIC_PUBLIC_REQUEST, "binance", "", "Check Server Time");
+    Request request(Request::Operation::GENERIC_PUBLIC_REQUEST, "binance", "", "Market Data");
     request.appendParam({
         {"HTTP_METHOD", "GET"},
-        {"HTTP_PATH", "/api/v3/time"},
+        {"HTTP_PATH", "/api/v3/exchangeInfo"},
     });
     session.sendRequest(request);
   }
-  std::this_thread::sleep_for(std::chrono::seconds(10));
+  std::this_thread::sleep_for(std::chrono::seconds(2));
   {
     // This endpoint requires X-MBX-APIKEY header: https://binance-docs.github.io/apidocs/spot/en/#old-trade-lookup
     // Therefore please set environment variable BINANCE_API_KEY.
-    Request request(Request::Operation::GENERIC_PUBLIC_REQUEST, "binance", "", "Old Trade Lookup");
+    Request request(Request::Operation::GENERIC_PUBLIC_REQUEST, "binance", "", "ETH/BTC Orderbook");
     request.appendParam({
         {"HTTP_METHOD", "GET"},
-        {"HTTP_PATH", "/api/v3/historicalTrades"},
-        {"HTTP_QUERY_STRING", "symbol=BTCUSDT"},
+        {"HTTP_PATH", "/api/v3/depth"},
+        {"HTTP_QUERY_STRING", "symbol=ETHBTC&limit=20"},
     });
     session.sendRequest(request);
   }
-  std::this_thread::sleep_for(std::chrono::seconds(10));
+  std::this_thread::sleep_for(std::chrono::seconds(2));
   session.stop();
   std::cout << "Bye" << std::endl;
   return EXIT_SUCCESS;
