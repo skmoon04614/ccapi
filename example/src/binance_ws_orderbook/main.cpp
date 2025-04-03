12891c039e97a86586e24a19857b34f83237d7f0
Sukki Moon <sukki.moon@schperics.com>
2025-04-03 19:20:26 +0900

    tmp: binance api 예제 추가

--- example/src/binance_ws_orderbook/main.cpp
+++ example/src/binance_ws_orderbook/main.cpp
@@ -2,7 +2,7 @@
 namespace ccapi {
 Logger* Logger::logger = nullptr;  // This line is needed.
 class MyEventHandler : public EventHandler {
- public:
+public:
   bool processEvent(const Event& event, Session* session) override {
     std::cout << "Received an event:\n" + event.toStringPretty(2, 2) << std::endl;
     return true;
@@ -17,6 +17,7 @@ using ::ccapi::SessionConfigs;
 using ::ccapi::SessionOptions;
 using ::ccapi::UtilSystem;
 using ::ccapi::UtilTime;
+using ::ccapi::Subscription;
 int main(int argc, char** argv) {
   if (UtilSystem::getEnvAsString("BINANCE_API_KEY").empty()) {
     std::cerr << "Please set environment variable BINANCE_API_KEY" << std::endl;
@@ -26,18 +27,14 @@ int main(int argc, char** argv) {
     std::cerr << "Please set environment variable BINANCE_API_SECRET" << std::endl;
     return EXIT_FAILURE;
   }
+
   SessionOptions sessionOptions;
   SessionConfigs sessionConfigs;
   MyEventHandler eventHandler;
   Session session(sessionOptions, sessionConfigs, &eventHandler);
-  Request request(Request::Operation::GENERIC_PRIVATE_REQUEST, "binance", "", "Get Account Info");
-  request.appendParam({
-      {"HTTP_METHOD", "GET"},
-      {"HTTP_PATH", "/api/v3/account"},
-      {"HTTP_QUERY_STRING", "recvWindow=60000"},
-  });
-  session.sendRequest(request);
-  std::this_thread::sleep_for(std::chrono::seconds(2));
+  Subscription subscription("binance", "", "GENERIC_PUBLIC_SUBSCRIPTION", R"({"method": "SUBSCRIBE", "params": ["btcusdt@depth20@100ms"], "id": 0})");
+  session.subscribe(subscription);
+  std::this_thread::sleep_for(std::chrono::seconds(15));
   session.stop();
   std::cout << "Bye" << std::endl;
   return EXIT_SUCCESS;
